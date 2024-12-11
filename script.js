8dc57021d2eb5f2bf43b71c432cc5132a601da60
nimratahir142 <khanzaadi142@gmail.com>
2024-12-11 18:36:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+// Create an Object:
+const firstPerson = {
+    firstname: "NIMRA" ,
+    lastname: "TAHIR" ,
+    age: 20 ,
+    colour: "hazelgray"
+}
+document.getElementById("one").innerHTML = firstPerson.firstname+" "+firstPerson.lastname+" "+firstPerson.age+" "+"year old"+" "+"and eyes colour"+" "+firstPerson.colour+"."
+// Create an 2ndObject:
+const secoundPerson = {
+    firstname: "AYESHA" ,
+    lastname: "LATIF" ,
+    age: 20 ,
+    colour: "brown"
+}
+document.getElementById("two").innerHTML = secoundPerson.firstname+" "+secoundPerson.lastname+" "+secoundPerson.age+" "+"year old"+" "+"and eyes colour"+" "+secoundPerson.colour+"."
+// Create an 3rdObject:
+const thirdPerson = {
+    firstname: "KHADIJA" ,
+    lastname: "FAIZ" ,
+    age: 20 ,
+    colour: "gray"
+}
+document.getElementById("three").innerHTML = thirdPerson.firstname+" "+thirdPerson.lastname+" "+thirdPerson.age+" "+"year old"+" "+"and eyes colour"+" "+thirdPerson.colour+"."
+// Create an Object:
+const car = {
+    type: "fait" ,
+    model: "500" ,
+    colour: "white" 
+}
+document.getElementById("four").innerHTML = "The car is type"+" "+car.type+" "+"and modle"+" "+car.model+" "+"and colour is"+" "+car.colour+" ."
+// Create an Object:
+const mahrancar = {
+    type: "mahran" ,
+    model: "500" ,
+    colour: "black" 
+}
+document.getElementById("five").innerHTML = "The car is type"+" "+mahrancar.type+" "+"and modle"+" "+mahrancar.model+" "+"and colour is"+" "+mahrancar.colour+" ."
+// Create an Object:
+const vagonRcar = {
+    type: "VagonR" ,
+    model: "500" ,
+    colour: "gray" 
+}
+document.getElementById("six").innerHTML = "The car is type"+" "+vagonRcar.type+" "+"and modle"+" "+vagonRcar.model+" "+"and colour is"+" "+vagonRcar.colour+" ."
+// Create an Object:
+const flower = {
+    type: "ROSE" ,
+    colour: "RED"
+}
+document.getElementById("seven").innerHTML = "The Floewr is type"+" "+flower.type+" "+"and colour is"+flower.colour+" "+"and its my so fvr8"+"."
+// Create an Object:
+const yellowflower = {
+    type: "ROSE" ,
+    colour: "YELLOW"
+}
+document.getElementById("eight").innerHTML = "The Floewr is type"+" "+yellowflower.type+" "+"and colour is"+yellowflower.colour+" "+"and its not my fvr8"+"."
+// Create an Object:
+const pinkflower = {
+    type: "ROSE" ,
+    colour: "PINK"
+}
+document.getElementById("nine").innerHTML = "The Floewr is type"+" "+pinkflower.type+" "+"and colour is"+pinkflower.colour+" "+"and its not my fvr8"+"."
+
+
